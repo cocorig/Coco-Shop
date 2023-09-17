adb87f368117537f3205c2b92e389ae57fc6814c
Hayeon LEE <fhdodl@naver.com>
2023-09-17 18:08:15 +0900

    Add : products data

--- src/data.js
+++ src/data.js
@@ -5,20 +5,87 @@ export const sliderItems = [
     img : '/assets/main/main-1.jpg',
     title : 'Chic Haven Accessories',
     desc : 'Discover elegance in every accessory',
-    bg : 'pink'
+
   },
   {
     id : 2 ,
     img : '/assets/main/main-2.jpg',
     title : 'Glamour Gems Emporium',
     desc : 'Adorn yourself with timeless treasures',
-    bg : 'pink'
+
   },
   {
     id : 3 ,
     img : '/assets/main/main-3.jpg',
     title : 'Trendy Trinkets Hub',
     desc : 'Find the latest must-have accessories here',
-    bg : 'pink'
+
+  },
+  { 
+    id : 4,
+    img : '/assets/main/main-4.jpg',
+    title : 'Elegance Adorned',
+    desc : 'Embrace sophistication with our accessories',
+
+  }
+]
+
+
+export const categories = [
+  {
+    id: 1,
+    img: '/assets/categories/bracele.jpg',
+    title: "Bracele",
+  
+  },
+  {
+    id: 2,
+    img: '/assets/categories/earrings.jpg',
+    title: "Earrings",
+  },
+  {
+    id: 3,
+    img:'/assets/categories/necklace.jpg',
+    title: "Necklace",
+  },
+  {
+    id: 4,
+    img:'/assets/categories/ring.jpg',
+    title: "Ring",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:'/assets/neck/necklace-2.jpg',
+  },
+  {
+    id:2,
+    img:'/assets/neck/necklace-3.jpg',
+  },
+  {
+    id:3,
+    img:'/assets/ring/ring-1.jpg',
+  },
+  {
+    id:4,
+    img:'/assets/ring/ring-2.jpg',
+  },
+  {
+    id:5,
+    img:'/assets/bracele/bracele-1.jpg',
+  },
+  {
+    id:6,
+    img:'/assets/bracele/bracele-2.jpg',
+  },
+  {
+    id:7,
+    img:'/assets/bracele/bracele-3.jpg',
+  },
+  {
+    id:8,
+    img:"/assets/earrings/earring-1.jpg",
   },
 ]
